9bb35776f4d85e0012797c35a93e05c5c5008578
yocosaka <sakayoco55@gmail.com>
2021-02-21 16:54:03 +0900

    Add functions to fetch the location data and weather data from API - Add dotenv-webpack and .env file to use token and id for APIs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
 import '../scss/main.scss';
 // import header from './mymodules/header';
 
-const accessToken = 'pk.eyJ1IjoieW9jb29ubyIsImEiOiJjazhtYXVrcGgwbHNjM2VwaDFvdjZncmZnIn0.8qVE_QjxtBJmTEAhwkBbIg';
-const appId = 'e59acf44e4a058aded4a5d6e10b4b052';
+const accessToken = process.env.MAPBOX_ACCESS_TOKEN;
+const appId = process.env.WEATHER_APP_ID;
 
 const location = async (location) => {
   const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${accessToken}`);
@@ -23,38 +23,30 @@ const weather = async (latitude, longtitude) => {
   throw new Error('Unable to fetch the weather data');
 };
 
-// const renderForecast = (placeName, weatherData) => {
-//   const container = document.querySelector('.weather__container');
+const renderForecast = (placeName, weatherData) => {
+  const container = document.getElementById('result');
+  const kToC = 273.15;
+  const temp = Math.round(weatherData.main.temp - kToC);
+  const { humidity } = weatherData.main;
+  const weather = weatherData.weather[0].main;
+  const { description } = weatherData.weather[0];
+  const { icon } = weatherData.weather[0];
+  const iconImgSrc = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
-//   // container.appendChild()
-// }
-
-// const kToC = 273.15;
-// const tempNow = Math.round(body.main.temp - kToC);
-// const humidity = body.main.humidity;
-// const weather = body.weather[0].main;
-// const description = body.weather[0].description;
-// const icon = body.weather[0].icon;
-// const iconImgSrc = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-// callback(
-//   undefined,
-//   `
-//   <div class="results__title">${description}</div>
-//   <img src="${iconImgSrc}" class="results__icon">
-//   <p>Today's weather is <span class="lowercase">${weather}</span>.</p>
-//   <p>It is currently ${tempNow} degrees celsius.</p>
-//   <p>Humidity is ${humidity}%.</p>
-// `
-// );
-
-
-const locationData = async () => {
-  try {
-    const locData = await location('tokyo');
-    const weatherData = await weather(locData.features[0].center[1], locData.features[0].center[0]);
-    console.log(weatherData);
-  } catch (error) {
-    console.log(error);
-  }
+  container.innerHTML = `
+  <div class="weather__place">${placeName}</div>
+  <div class="weather__title">${weather}</div>
+  <img src="${iconImgSrc}" alt="${weather}" class="weather__image">
+  <div class="weather__desc">${description}</div>
+  <p class="weather__text">It is currently <span>${temp}</span>&#8451;. Humidity is <span>${humidity}</span>%.</p>`;
 };
-locationData();
+
+document.getElementById('search-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const keyword = await e.target.elements.locName.value.toLowerCase();
+  const locData = await location(keyword);
+  const weatherData = await weather(locData.features[0].center[1], locData.features[0].center[0]);
+  renderForecast(locData.features[0].place_name, weatherData);
+  document.querySelector('.weather').classList.add('open');
+  e.target.elements.locName.value = '';
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
@@ -49,4 +50,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new Dotenv(),
+  ],
 };

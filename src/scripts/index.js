e3bad0da36c33fec3cac974adacccafb166db433
yocosaka <sakayoco55@gmail.com>
2021-02-21 13:39:45 +0900

    Add initial files

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import '../scss/main.scss';
+// import header from './mymodules/header';
+// import footer from './mymodules/footer';
+// import main from './mymodules/main';
+// import menu from './mymodules/menu';
+// import addTabEvent from './mymodules/tab';
+
+// header();

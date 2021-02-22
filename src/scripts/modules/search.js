8265a4d80013ebcf37947eda6921fe59134aa2d9
yocosaka <sakayoco55@gmail.com>
2021-02-22 16:41:45 +0900

    Add renderErrors function to handle the case of not found the city

--- src/scripts/modules/search.js
+++ src/scripts/modules/search.js
@@ -3,6 +3,11 @@ import weather from './weather';
 import renderWeather from './render';
 import photo from './photo';
 
+const renderError = () => {
+  const errorContainer = document.getElementById('error-msg');
+  errorContainer.textContent = 'Sorry... The city was not found.';
+};
+
 const displayBackgroundImage = async (keyword) => {
   const photoPath = await photo(keyword);
   const body = document.querySelector('body');
@@ -18,11 +23,14 @@ const search = () => {
       const locData = await location(inputQuery);
       // eslint-disable-next-line max-len
       const weatherData = await weather(locData.features[0].center[1], locData.features[0].center[0]);
+      document.getElementById('error-msg').innerHTML = '';
       renderWeather(locData.features[0].place_name, weatherData, e.target.elements.tempUnit.value);
       document.querySelector('.weather').classList.add('open');
       displayBackgroundImage(weatherData.weather[0].main);
     } catch (error) {
+      renderError();
       document.getElementById('loading').classList.remove('play');
+      document.getElementById('result').innerHTML = '';
     }
     e.target.elements.locName.value = '';
   });
